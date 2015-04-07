679b131c9e62642974d3cc7eb64f86d1c306b06e
lun471k <jfnoel@Keybook-P19.cellulaireplus.com>
2015-04-07 16:56:13 -0400

    Added zoom detection for IE and Firefox
    Added zoom detection for IE and Firefox

--- src/zoomDetector.js
+++ src/zoomDetector.js
@@ -1,11 +1,17 @@
 // This function returns false if the browser is not zoomed.
 // Otherwise it returns the zoom ratio.
 function zoomDetector() {
-    if(screen.deviceXDPI){
-        return screen.deviceXDPI !== screen.logicalXDPI ? screen.logicalXDPI / screen.deviceXDPI : false;
+    //Firefox
+    if(window.devicePixelRatio){
+        return window.devicePixelRatio !== 1 ? window.devicePixelRatio : false;
     }
+    // IE8+
+    if(screen.deviceXDPI){
+        return screen.deviceXDPI !== screen.logicalXDPI ? screen.deviceXDPI / screen.logicalXDPI : false;
+    }
+    //Webkit
     if(window.innerWidth !== window.outerWidth) {
-        return window.innerWidth / window.outerWidth;
+        return window.outerWidth / window.innerWidth;
     }
     return false;
 }
